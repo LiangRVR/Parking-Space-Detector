d2ac3cc226d94ac6f0bb17002825a5d608090bfb
LiangRVR <liangvillarrubia95@gmail.com>
2023-11-10 12:19:26 -0500

    Remove unnecessary print statement in draw_rectangles function

--- util/utils.py
+++ util/utils.py
@@ -4,7 +4,6 @@ from util.colors import COLOR_BLUE, COLOR_RED
 def draw_rectangles(points, image, is_occupied = False):
     point_1 = points[0]
     color = COLOR_BLUE if not is_occupied else COLOR_RED
-    print(color)
     for i in range(1, len(points)):
         cv2.line(image, point_1, points[i], color, 2)
         point_1 = points[i]
